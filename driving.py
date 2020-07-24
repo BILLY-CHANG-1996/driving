ad213a595795a704e580a6d43aa705f39eee2de4
BILLY CHANG <ts04574870@gmail.com>
2020-07-24 21:32:06 +0900

    update version

--- driving.py
+++ driving.py
@@ -4,12 +4,14 @@ age = int(age)
 if country == '台灣':
     if age >= 18:
         print('你可以考駕照了')
-    if age <18:
+    else:
+        age < 18
         print('你還不能考駕照喔')
-if country == '美國':
+elif country == '美國':
     if age >= 16:
         print('你可以考駕照了')
-    if age < 16:
+    else:
+        age < 16
         print('你還不能考駕照喔')
         
         
