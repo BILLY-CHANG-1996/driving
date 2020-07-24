49d7f36a6361b6c992d1c0591119ffb843e358f8
BILLY CHANG <ts04574870@gmail.com>
2020-07-24 21:55:33 +0900

    update version

--- driving.py
+++ driving.py
@@ -1,19 +1,15 @@
 country = input('請問你是哪國人: ')
+if country != '台灣' and country != '美國':
+    print('你只能輸入台灣或美國喔!!')
 age = input('請問你幾歲: ')
 age = int(age)
 if country == '台灣':
     if age >= 18:
         print('你可以考駕照了')
     else:
-        age < 18
-          print('你還不能考駕照喔')
+        print('你還不能考駕照喔')
 elif country == '美國':
     if age >= 16:
           print('你可以考駕照了')
     else:
-        age < 16
-          print('你還不能考駕照喔')
-        
-        
-else:
-    print('你只能輸入台灣或美國喔!!')
+        print('你還不能考駕照喔')                
